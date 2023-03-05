b70f76f26f9a44f67681aff062d0881965539e19
dnielpy <dnielquesada@gmail.com>
2023-03-05 21:39:02 +0100

    Ejercicio 2 CP Funciones

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
-﻿class Program
+﻿using System;
+using System.Diagnostics;
+
+class Program
 {
     static void Main(){
-        static int ecuacionCuadratica(){
+        // Implemente una funcion que cuando se invoque, espere una entrada´ de texto y devuelva la velocidad de escritura del usuario.
+        static double writeSpeed(){
+            Stopwatch crono = new Stopwatch();
+            Console.WriteLine("Escribe un texto: ");
 
+            crono.Start();
+            string texto = Console.ReadLine();
+            crono.Stop();
 
+            double ms = (double) crono.ElapsedMilliseconds;
 
-            return 1;
+            return ms;
         }
+
+        Console.WriteLine($"Te has demorado: {writeSpeed()} ms");
+
+    
     }
 }
