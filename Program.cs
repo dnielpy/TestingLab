aa595e26b8eaa14631f15dfe79e0fad3051f9094
dnielpy <dnielquesada@gmail.com>
2023-03-12 12:43:55 -0500

    Ejercicio de El mayor de un array resuelto

--- Program.cs
+++ Program.cs
@@ -1,51 +1,22 @@
 ﻿using System;
-using System.Diagnostics;
 
 class Program
 {
     static void Main(){
 
-        int agrego = 0;
-
-        static void Write(string texto){
-            Console.WriteLine(texto);
-        };
-
-//Minutes Of Flight
-        static int minutesOfFlight(int MinA, int MinB){          
-            int minutes = (60 - MinA) + MinB;
-            return minutes;
-        }
-
-//Hours Of Flights
-        static int hoursOfFlights(int HourA, int HourB, int agrego){
-            int hour = Math.Max(HourA, HourB) - Math.Min(HourA, HourB) + agrego;
-            return hour;
-        }
-
-//Time Of Flight
-        static string timeOfFlight(int horas, int minutos){
-            if (minutos == 60)
+        //Imlementar un metodo que devuelva el mayor de un array
+        static int Mayor(int[] array){
+            int maxValue = 0;
+            for (int i = 0; i < array.Length; i++)
             {
-                minutos = 60 - minutos;
-            } 
-            string time = $"{horas}:{minutos}";
-            return time;
+                maxValue = Math.Max(array[i], maxValue);
+            }
+            System.Console.WriteLine(maxValue);
+            return 0;
         }
+        int[] miArray = {1,20,3,4,5};
+        Mayor(miArray);
 
-        Write("Escribe la hora A: ");
-        int hora = int.Parse(Console.ReadLine());
-        Write("Escribe los minutos A: ");
-        int mina = int.Parse(Console.ReadLine());
-        Write("Escribe la hora B: ");
-        int horb = int.Parse(Console.ReadLine());
-        Write("Escribe los minutos B: ");
-        int minb = int.Parse(Console.ReadLine());
 
-        if (minutesOfFlight(mina, minb) > 60){
-            agrego++;
-        }
-
-        Console.WriteLine(timeOfFlight(hoursOfFlights(hora, horb, agrego), minutesOfFlight(mina, minb)));
     }
 }
